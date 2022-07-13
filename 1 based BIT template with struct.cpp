f84eaf40a3db79c7a54e9bc1dfa207a56fc9bf2f
Md. Saimoon Islam <ash1911025m@gmail.com>
2022-07-13 21:00:48 +0600

    Create 1 based BIT template with struct.cpp

---
+++ 1 based BIT template with struct.cpp
@@ -0,0 +1,26 @@
+typedef long long ll;
+template< typename T >
+struct fenwick
+{
+    ll n;
+    vector<T> bit;
+    fenwick(ll x, T init) {
+        n = x;
+        bit.assign(n, init);
+    }
+    void update(int indx, T val) {
+        for ( ; indx <= n; indx += indx & -indx) {
+            bit[indx] += val;
+        }
+        return ;
+    }
+    T query(int x) {
+        T sum = 0;
+        for ( ; x > 0; x -= x & -x) {
+            sum += bit[x];
+        }
+        return sum;
+    }
+};
+// initialization
+//fenwick<ll> tree1(10, 0);
