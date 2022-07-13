fb01b4b372ae55803b8883f73ee5d23a59b5e7df
Md. Saimoon Islam <ash1911025m@gmail.com>
2022-07-13 21:46:28 +0600

    Update 0 based binary indexed tree.cpp

--- 0 based binary indexed tree.cpp
+++ 0 based binary indexed tree.cpp
@@ -1,37 +1,46 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-int n;
-int bit[100001];
-void update(int indx,int val){
-    for( ;indx<n;indx=indx|(indx+1)){ // for 0 based array
-        bit[indx]+=val;
+template< typename T >
+struct fenwick
+{
+    ll n;
+    vector<T> bit;
+    fenwick(ll x, T init) {
+        n = x;
+        bit.assign(n, init);
     }
-    return ;
-}
-
-int query(int x){
-    int sum=0;
-    for( ;x>=0;x=(x&(x+1))-1){
-        sum+=bit[x];
+    void update(int indx, T val) {
+        for ( ; indx < n; indx += indx & -indx) {
+            bit[indx] += val;
+        }
+        return ;
     }
-    return sum;
-}
+    T query(int x) {
+        T sum = 0;
+        for ( ; x >= 0; x -= x & -x) {
+            sum += bit[x];
+        }
+        return sum;
+    }
+};
+//fenwick<ll> tree1(10, 0);
 int main()
 {
     int a,q;
     cin>>n>>q;
+    fenwick<ll> tree1(n,0);
     for(int i=0;i<n;i++){
         cin>>a;
-        update(i,a);
+        tree1.update(i,a);
     }
     ll l,r;
     for(int i=0;i<q;i++){
         cin>>l>>r;
         l--;r--;
         //becareful to handle the case with 0 
-        cout<<query(r)-query(l-1)<<endl;
+        cout<<tree1.query(r)-tree1.query(l-1)<<endl;
     }
     return 0;
 }
-//  reffernce : hackerrearth
+//  reffernce : hackerrearth and cp-algorithms
