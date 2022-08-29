229b7375a8bdc77027531a86d5567e4db8a2d1f4
Md. Saimoon Islam <ash1911025m@gmail.com>
2022-08-30 00:05:40 +0600

    Update Longest Increasing Subsequence.cpp

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -31,3 +31,51 @@ ll lis(vector<ll> &a) {
     return ret;
 }
 // reference : cp-algorithms
+
+
+
+// non-decreasing
+
+ll bs(vector<ll>&d, ll x) {
+    ll low = 0, high = d.size() -1;
+    ll out;
+    while (low <= high) {
+        ll mid = (low + high) / 2;
+        if (d[mid] <= x) {
+            low = mid + 1;
+        }
+        else high = mid - 1, out = mid;
+    }
+    return out;
+}
+ll lis(vector<ll> &a) {
+    ll n = a.size();
+    vector<ll> d(n+1, inf), p(n+1, -1), index(n+1, -1);
+    d[0] = -inf;
+    for (int i = 0; i < n; i++) {
+        int id = bs(d,a[i]);
+        if (d[id-1] <= a[i] and d[id] > a[i]) {
+            d[id] = a[i];
+            index[id] = i;
+            p[i] = index[id-1];
+        }
+    }
+    ll ret = -1;
+    for (int i = n; i >= 1; i--) {
+        if (d[i] != inf) {
+            ret = i;
+            break;
+        }
+    }
+    ll now = index[ret];
+    vector<ll> sequence;
+    while (now != -1) {
+        sequence.push_back(a[now]);
+        now = p[now];
+    }
+    for (int i = ret - 1; i >= 0; i--) {
+        cout << sequence[i] << ' ';
+    }
+    cout << endl;
+    return ret;
+}
